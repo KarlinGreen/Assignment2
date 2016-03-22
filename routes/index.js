1c7e2387d31078350edff6330db0a091d836397b
KarlinGreen <kcgreen@rogers.com>
2016-03-21 20:28:50 -0400

    Update 2 - IMPORTANT
    Fixed app.js so that the app  does NOT throw an error  in the  console.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pub Stop' });
 });
 
+//GET Pub List
+router.get('/pubList', function(req, res, next){
+  res.render('pubList', { title: 'Pub List'});
+});
+
 module.exports = router;
